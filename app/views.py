577bfe4aef110029bb2db09f7a04eb76aafd7a32
jamoncrief <james.adam.moncrief@gmail.com>
2016-12-14 19:18:31 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,18 +1,51 @@
 from app import app
-from flask import render_template, flash, redirect, request, url_for, jsonify
+from flask import render_template, flash, redirect, request, url_for, jsonify, make_response
 from math import sqrt
+
+
 						
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	#form = DataForm()
-	#if form.validate_on_submit():
-	#	clicks = form.Data1.data
-	#	conversions = form.Data2.data
-	#	cpc = form.Data3.data
-	#	totalsales = form.Data4.data
-	#	return render_template('home.html')
-	return render_template('home.html')#, form=form)
+		return render_template('home.html')
+		
+@app.route('/count', methods=['GET', 'POST'])
+def count():
+	if 'count' in request.cookies:
+			flash('Create an account to continue using the PPC ACOS Interval Tool')
+			return render_template('signup.html')
+	else:
+		response = make_response(redirect(url_for('count')))
+		response.set_cookie('count','1')
+		flash('cookie created')
+		return render_template('home.html')
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+    # # Here we use a class of some kind to represent and validate our
+    # # client-side form data. For example, WTForms is a library that will
+    # # handle this for us, and we use a custom LoginForm to validate.
+    # form = LoginForm()
+    # if form.validate_on_submit():
+        # # Login and validate the user.
+        # # user should be an instance of your `User` class
+        # login_user(user)
+
+        # flask.flash('Logged in successfully.')
+
+        # next = flask.request.args.get('next')
+        # # is_safe_url should check if the url is safe for redirects.
+        # # See http://flask.pocoo.org/snippets/62/ for an example.
+        # if not is_safe_url(next):
+            # return flask.abort(400)
+
+        # return flask.redirect(next or flask.url_for('index'))
+    # return flask.render_template('login.html', form=form)
 	
+	
+#@app.route('/setcount', methods=['GET', 'POST'])
+#def setcount():
+#	count = request.get.
+
 @app.route('/_ACOSintervalb', methods=['GET', 'POST'])
 def Calculate_ACOS_intervalb():
 	clicks = request.args.get('clicks', 0, type=int)
@@ -60,3 +93,4 @@ def Calculate_ACOS_intervalw():
 	ACOSworst = round(ACOSworst, 1)
 	
 	return jsonify(resultw = ACOSworst)
+	
