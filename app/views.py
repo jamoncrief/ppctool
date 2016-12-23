39acbd0a47f45fd20673f2ebb5dc9572f3602ab0
jamoncrief <james.adam.moncrief@gmail.com>
2016-12-23 14:56:05 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,45 +1,86 @@
-{% extends "base.html" %}
-{% block content %}
+from app import app
+from flask import render_template, flash, redirect, request, url_for, jsonify, make_response
+from math import sqrt
 
-<h3> Sign in With Amazon </h3>
+import pycurl
+import urllib
+import json
+import io
+						
+@app.route('/', methods=['GET', 'POST'])
+def home():
+		return render_template('home.html')
+		
+@app.route('/privacypolicy', methods=['GET', 'POST'])
+def privacy():
+		return render_template('privacypolicy.html')
+		
+# @app.route('/count', methods=['GET', 'POST'])
+# def count():
+	# if 'count' in request.cookies:
+			# flash('Create an account to continue using the PPC ACOS Interval Tool')
+			# return render_template('signup.html')
+	# else:
+		# response = make_response(redirect(url_for('count')))
+		# response.set_cookie('count','1')
+		# flash('cookie created')
+		# return render_template('home.html')
+		
 
+ 
+@app.route('/login', methods=['GET', 'POST'])
+def loginwithamazon():
+	return render_template('login.html')
+	
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+	return render_template('dashboard.html')
 
-<a href="#" id="LoginWithAmazon">
-  <img border="0" alt="Login with Amazon"
-    src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png"
-    width="156" height="32" />
-</a>
+@app.route('/_ACOSintervalb', methods=['GET', 'POST'])
+def Calculate_ACOS_intervalb():
+	clicks = request.args.get('clicks', 0, type=int)
+	conversions = request.args.get('conversions', 0, type=int)
+	cpc = request.args.get('cpc', 0, type=float)
+	totalsales = request.args.get('totalsales', 0, type=float)
+	totalspend = request.args.get('totalspend', 0, type=float)
+	
+	if clicks == 0:
+		return 0
 
-<body>
-	<div id="amazon-root"></div>
-<script type="text/javascript">
+	z = 1.96 #1.44 = 85%, 1.96 = 95%
+	phat = conversions / clicks
+	plow = ((phat + z*z/(2*clicks) - z * sqrt((phat*(1-phat)+z*z/(4*clicks))/clicks))/(1+z*z/clicks))
+	phigh = ((phat + z*z/(2*clicks) + z * sqrt((phat*(1-phat)+z*z/(4*clicks))/clicks))/(1+z*z/clicks))
+	
+		
+	ACOSbest = ((cpc - 0.05 * cpc) / ((totalsales / conversions * phigh)))*100
+	ACOSworst = ((cpc + 0.05 * cpc) / ((totalsales / conversions * plow)))
+	
+	ACOSbest = round(ACOSbest, 1)
+	
+	return jsonify( resultb = ACOSbest)
 
-  window.onAmazonLoginReady = function() {
-    amazon.Login.setClientId('amzn1.application-oa2-client.6537ed54f7d64209a6b4254163d58138');
-  };
-  (function(d) {
-    var a = d.createElement('script'); a.type = 'text/javascript';
-    a.async = true; a.id = 'amazon-login-sdk';
-    a.src = 'https://api-cdn.amazon.com/sdk/login1.js';
-    d.getElementById('amazon-root').appendChild(a);
-  })(document);
+@app.route('/_ACOSintervalw', methods=['GET', 'POST'])
+def Calculate_ACOS_intervalw():
+	clicks = request.args.get('clicks', 0, type=int)
+	conversions = request.args.get('conversions', 0, type=int)
+	cpc = request.args.get('cpc', 0, type=float)
+	totalsales = request.args.get('totalsales', 0, type=float)
+	totalspend = request.args.get('totalspend', 0, type=float)
+	
+	if clicks == 0:
+		return 0
 
-</script>
-
-<script type="text/javascript">
-
-  document.getElementById('LoginWithAmazon').onclick = function() {
-    options = { scope : 'profile', popup: true };
-    amazon.Login.authorize(options, 'https://ppctool.herokuapp.com/dashboard');
-    return false;
-  };
-
-</script>
-
-<script type="text/javascript">
-  document.getElementById('Logout').onclick = function() {
-    amazon.Login.logout();
-};
-</script>
-
-{% endblock %}
+	z = 1.96 #1.44 = 85%, 1.96 = 95%
+	phat = conversions / clicks
+	plow = ((phat + z*z/(2*clicks) - z * sqrt((phat*(1-phat)+z*z/(4*clicks))/clicks))/(1+z*z/clicks))
+	phigh = ((phat + z*z/(2*clicks) + z * sqrt((phat*(1-phat)+z*z/(4*clicks))/clicks))/(1+z*z/clicks))
+	
+		
+	ACOSbest = ((cpc - 0.05 * cpc) / ((totalsales / conversions * phigh)))
+	ACOSworst = ((cpc + 0.05 * cpc) / ((totalsales / conversions * plow)))*100
+	
+	ACOSworst = round(ACOSworst, 1)
+	
+	return jsonify(resultw = ACOSworst)
+	
