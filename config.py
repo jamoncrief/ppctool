1e0471c7d387829d8d21bb64ad825f90d2540d68
jamoncrief <james.adam.moncrief@gmail.com>
2017-03-26 21:36:39 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'monchief'
 
+GOOGLE_LOGIN_CLIENT_ID = "603499396792-bl8scdsmlncnjiff8l61f0e8l6li3din.apps.googleusercontent.com"
+GOOGLE_LOGIN_CLIENT_SECRET = "t2hCcF5NCR5Iq3sewc1T67aQ"
+
+OAUTH_CREDENTIALS={
+        'google': {
+            'id': GOOGLE_LOGIN_CLIENT_ID,
+            'secret': GOOGLE_LOGIN_CLIENT_SECRET
+        }
+}
 
 
 import os
