fdc4cfbadcf3d3e4f8166176743b7a51d46a2b68
jamoncrief <james.adam.moncrief@gmail.com>
2017-03-26 21:52:07 -0600

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,8 @@ OAUTH_CREDENTIALS={
         }
 }
 
-
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
