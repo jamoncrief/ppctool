f439604b09a6129ee0342c9e585469017e9b7321
jamoncrief <james.adam.moncrief@gmail.com>
2017-03-27 21:19:19 -0600

    Update config.py

--- config.py
+++ config.py
@@ -14,5 +14,16 @@ OAUTH_CREDENTIALS={
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') +
+                               '?check_same_thread=False')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
+
+# slow database query threshold (in seconds)
+DATABASE_QUERY_TIMEOUT = 0.5
+
+# administrator list
+ADMINS = ['james.adam.moncrief@gmail.com']
