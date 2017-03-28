2f3cd4899645619913bf0ded2f768f2cc7513132
jamoncrief <james.adam.moncrief@gmail.com>
2017-03-27 21:13:27 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'monchief'
 
-
-
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
